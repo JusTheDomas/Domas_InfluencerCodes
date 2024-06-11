a0d69853fddf60cd559ea7dece47845f606c333b
JusTheDomas <76823865+JusTheDomas@users.noreply.github.com>
2024-06-11 21:12:26 +0300

    Update config.lua
    Added auto framework detect + QB core functions

--- config.lua
+++ config.lua
@@ -5,8 +5,8 @@ Config.Database = 'oxmysql' -- oxmysql/mysql-async/ghmattimysql
 Config.Debug = false -- Enable debug
 Config.DebugSQL = false -- Enable SQL debug
 Config.DiscordLog = true -- Enable discord logging
-Config.AllowOnlyOne = true -- if true, player will be able to use only 1 code
-Config.Webhook = 'https://discord.com/api/webhooks/1226478697415053342/HPxf2dBbDkDogLmsaXjKFj5W5oWg80Oe7TKpCFLD4aC4Mj6aXXYbJIqeqKGO-bmkgb4j'
+Config.AllowOnlyOne = false -- if true, player will be able to use only 1 code
+Config.Webhook = 'https://discord.com/api/webhooks/...'
 
 Config.CommandName = 'code'
 
@@ -43,24 +43,54 @@ Config.Text = {
     
 }
 
-function AddMoney(playerId, amount) -- Add your own logic if not using ESX
-    ESX = exports["es_extended"]:getSharedObject()
-    local xPlayer = ESX.GetPlayerFromId(playerId)
-    xPlayer.addMoney(amount)
-    Debug("Giving money: "..amount.." to player: "..playerId)
-    Notify("You used code and got "..amount..' €', playerId)
+function AddMoney(playerId, amount)
+    if GetResourceState('es_extended') == 'started' then
+        Debug("You're using ESX!")
+        ESX = exports["es_extended"]:getSharedObject()
+        local xPlayer = ESX.GetPlayerFromId(playerId)
+        xPlayer.addMoney(amount)
+        Debug("Giving money: "..amount.." to player: "..playerId)
+        Notify("You used code and got "..amount..' €', playerId)
+    elseif GetResourceState('qb-core') == 'started' then 
+        Debug("You're using QB-Core!")
+        QBCore = exports['qb-core']:GetCoreObject()
+        local Player = QBCore.Functions.GetPlayer(playerId)
+        Player.Functions.AddMoney('cash', amount, 'Admin give money')
+        Debug("Giving money: "..amount.." to player: "..playerId)
+        Notify("You used code and got "..amount..' €', playerId)
+    else
+        Debug("You're using non supported framework, please edit the functions (AddMoney) for rewards at config.lua file! (line 62)")
+    end
 end
 
 
-function GiveItem(playerId, itemName, quantity) -- Add your own logic if not using ESX
-    ESX = exports["es_extended"]:getSharedObject()
-    local xPlayer = ESX.GetPlayerFromId(playerId)
-    print("playerId: "..playerId)
-    xPlayer.addInventoryItem(itemName, quantity)
-    Debug("Giving item: "..itemName.." x"..quantity.." to player: "..playerId)
-    Notify("You used code and got "..itemName, playerId)
+function GiveItem(playerId, itemName, quantity)
+    if GetResourceState('es_extended') == 'started' then
+        Debug("You're using ESX!")
+        ESX = exports["es_extended"]:getSharedObject()
+        local xPlayer = ESX.GetPlayerFromId(playerId)
+        xPlayer.addInventoryItem(itemName, quantity)
+        Debug("Giving item: " .. itemName .. " x" .. quantity .. " to player: " .. playerId)
+        Notify("You used code and got " .. itemName, playerId)
+    elseif GetResourceState('qb-core') == 'started' then
+        Debug("You're using QB-Core!")
+        QBCore = exports['qb-core']:GetCoreObject()
+        local Player = QBCore.Functions.GetPlayer(playerId)
+        Player.Functions.AddItem(itemName, quantity)
+        Debug("Giving item: " .. itemName .. " x" .. quantity .. " to player: " .. playerId)
+        Notify("You used code and got " .. itemName, playerId)
+    else
+        Debug("You're using non supported framework, please edit the functions (GiveItem) for rewards at config.lua file! (line 83)")
+    end
 end
+
 
 function Notify(text, playerId)
-    TriggerClientEvent('esx:showNotification', playerId, text, 'info', 7000)
+    if GetResourceState('es_extended') == 'started' then
+        TriggerClientEvent('esx:showNotification', playerId, text, 'info', 7000)
+    elseif GetResourceState('qb-core') == 'started' then
+        TriggerClientEvent('QBCore:Notify', playerId, text)
+    else
+        Debug("You're using non supported framework, please edit the functions (Notify) for rewards at config.lua file! (line 94)")
+    end
 end
